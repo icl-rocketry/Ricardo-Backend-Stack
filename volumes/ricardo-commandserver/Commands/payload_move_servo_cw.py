a8f844d1ec1868733a5f294ba944005651b60938
Andrei <paduraru.andrei5@gmail.com>
2024-10-11 01:43:52 +0100

    payload move servo commands

---
+++ volumes/ricardo-commandserver/Commands/payload_move_servo_cw.py
@@ -0,0 +1,41 @@
+from CommandServer.commandserver import CommandServer
+from cmd2 import Cmd2ArgumentParser
+
+import time
+
+payload_move_servo_cw_ap = Cmd2ArgumentParser()
+payload_move_servo_cw_ap.add_argument("--destination_service",type=int,required=True)
+payload_move_servo_cw_ap.add_argument('--duration',type=float,required=False,default=1)
+
+@CommandServer.register('payload_move_payload_move_servo_cw',argparse=payload_move_servo_cw_ap)
+def payload_move_servo_cw(instance,args):
+
+    command_packet_args = {"source":1,
+                        "source_service":instance.source_service,
+                        "destination":200,
+                        "destination_service":20,
+                        "command_id":2,
+                        "command_arg":70}
+
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+    time.sleep(args.get('duration',1))
+    command_packet_args['command_arg'] = 90
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+    time.sleep(0.02)
+    instance.send_command_packet(command_packet_args)
+
+    return True
