b4fc3dc588d4cb97c31648e4f92a484e51e26b00
Andrei <paduraru.andrei5@gmail.com>
2024-12-12 21:00:08 +0000

    stuff from igniter test

--- volumes/ricardo-commandserver/Commands/thanos_disarm.py
+++ volumes/ricardo-commandserver/Commands/thanos_disarm.py
@@ -10,7 +10,7 @@ def thanos_disarm(instance,args):
 
     command_packet_args = {"source":1,
                         "source_service":instance.source_service,
-                        "destination":10,
+                        "destination":2,
                         "destination_service":10,
                         "command_id":4,
                         "command_arg":0}
--- volumes/ricardo-commandserver/Commands/thanos_execute.py
+++ volumes/ricardo-commandserver/Commands/thanos_execute.py
@@ -11,7 +11,7 @@ def thanos_execute(instance,args):
 
     command_packet_args = {"source":1,
                         "source_service":instance.source_service,
-                        "destination":10,
+                        "destination":2,
                         "destination_service":10,
                         "command_id":2,
                         "command_arg":args['argument']}
